d800bd52de72686aef82190475e32b8dacb95b09
eunbin.choi <choiqls43@gmail.com>
2025-06-19 21:40:46 +0900

    django

--- mydjango_0618/config/urls.py
+++ mydjango_0618/config/urls.py
@@ -1,23 +1,18 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include  # ✅ include 추가되어야 함
+from django.urls import path, include
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("chat/", include("chat.urls")),  # ✅ chat.urls 포함 필요!
+    path('admin/', admin.site.urls),
+    # chat/urls에 있는 모든 URL 패턴에 일괄적으로
+    #  chat/ 라는 prefix 주소를 부여하겠다.
+    path('chat/', include('chat.urls')),
 ]
+
+# 유저가 업로드한 파일을 서빙하는 View 설정 (View <- 장고 개발서버에서 제공)
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
+
+urlpatterns += debug_toolbar_urls()
